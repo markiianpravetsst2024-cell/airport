03324946a251823ad8f8c07a4230fd092ab01e48
Markiian <markpravets1@gmail.com>
2026-07-17 18:06:18 +0300

    Backup before restructuring apps

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.models import User  # Імпортуємо стандартну модель
 
-# Register your models here.
+
+admin.site.register(User)
