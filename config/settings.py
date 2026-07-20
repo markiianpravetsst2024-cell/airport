a8562b11ec1c4b49cf1472d156c87ae470c8de63
Markiian <markpravets1@gmail.com>
2026-07-20 22:01:09 +0300

    Implement CRUD endpoints for all apps

--- config/settings.py
+++ config/settings.py
@@ -87,14 +87,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': 'airport_db',
+        'USER': 'myuser',
+        'PASSWORD': 'markdoc15',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Flight, Ticket
+from .serializers import FlightSerializer, TicketSerializer
 
-# Create your views here.
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
